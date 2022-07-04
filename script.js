c12b61bceeec50fec7d1ece427db6ee9611abb61
Renuka Prasad <renu.yarasu@gmail.com>
2022-07-04 21:57:10 +0530

    Loops - for

--- script.js
+++ script.js
@@ -1,25 +1,9 @@
 // Laurence Svekis - JavaScript Lessons
 console.clear();
 
-// JavaScript Functions
+// Loops
 
-function loop(val) {
-    console.log(val);
-    if (val > 0) {
-        val--;
-        loop(val)
-    }
-    return 'End'
+for (let x = 0; x < 5; x++) {
+    console.log(x);
 }
-loop(10)
 
-/* 9
-8
-7
-6
-5
-4
-3
-2
-1
-0 */
