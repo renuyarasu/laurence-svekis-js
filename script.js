730b75f613c6739bda9a5d51193d2fc8786643b0
Renuka Prasad <renu.yarasu@gmail.com>
2022-07-04 21:12:27 +0530

    Functions Recursion - Ex 02

--- script.js
+++ script.js
@@ -5,8 +5,11 @@ console.clear();
 
 function loop(val) {
     console.log(val);
-    if (val < 1) { return }
-    loop(val - 1);
+    if (val > 0) {
+        val--;
+        loop(val)
+    }
+    return 'End'
 }
 loop(10)
 
