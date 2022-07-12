7e590726a45a95088f5f54e5d3ae3e2e0cd54e1a
Renuka Prasad <renu.yarasu@gmail.com>
2022-07-12 17:02:00 +0530

    Math.floor()

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // Laurence Svekis - JavaScript Lessons
 console.clear();
 
-// Math Random
-let maths = Math.random()
-console.log(maths * 100);
+// Math Random/Floor
+
+for (let i = 0; i < 100; i++) {
+    console.log(randumNumber(1, 10));
+}
+
+function randumNumber(min, max) {
+    return Math.floor(Math.random() * max) + min
+}
