70a35c36c160570f4849456da363af766c3be7df
Renuka Prasad <renu.yarasu@gmail.com>
2022-07-12 10:43:27 +0530

    Objects - forEach()

--- script.js
+++ script.js
@@ -2,8 +2,12 @@
 console.clear();
 
 // Objects 
-const person = {
-    name: 'VedaGna',
-    age: 5
-} 
-console.log(person.age);
+const person_01 = { name: 'VedaGna', surname: 'Yarasu', age: 5 }
+const person_02 = { name: 'Hindu', surname: 'Yarasu', age: 25 }
+const person_03 = { name: 'Gnapika', surname: 'Yarasu', age: 7 }
+const person_04 = { name: 'Renu', surname: 'Yarasu', age: 35 }
+
+const people = [person_01, person_02, person_03];
+people.forEach((person) => {
+    console.log(`${person.name} ${person.surname}`);
+})
