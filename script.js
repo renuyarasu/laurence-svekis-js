c8bb5bf391886af3bf0399dc6d8bf99f349e2864
Renuka Prasad <renu.yarasu@gmail.com>
2022-07-12 16:50:08 +0530

    Math.random()

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
 // Laurence Svekis - JavaScript Lessons
 console.clear();
 
-// Objects 
-const person_01 = { name: 'VedaGna', surname: 'Yarasu', age: 5 }
-const person_02 = { name: 'Hindu', surname: 'Yarasu', age: 25 }
-const person_03 = { name: 'Gnapika', surname: 'Yarasu', age: 7 }
-const person_04 = { name: 'Renu', surname: 'Yarasu', age: 35 }
-
-const people = [person_01, person_02, person_03];
-people.forEach((person) => {
-    console.log(`${person.name} ${person.surname}`);
-})
+// Math Random
+let maths = Math.random()
+console.log(maths * 100);
