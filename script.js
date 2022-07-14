c546fee76d29e24eb4770b1542aa13c35f6b2c83
Renuka Prasad <renu.yarasu@gmail.com>
2022-07-14 20:30:00 +0530

    Objects and Arrays

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 // Laurence Svekis - JavaScript Lessons
 console.clear();
+// https://www.youtube.com/watch?v=8wLOyb_KOVw&list=PLTPx7x7O-7NCv6gWGvEfJc9OfQnxFPML-&index=16
+// JavaScript Lesson How to Output JavaScript Objects and Arrays to a web page JSON
 
-// Template literals
+const output = document.querySelector('.output');
 
-let name = 'VedaGna';
-let surename = 'Yarasu';
+const myObject = {
+    people: [
+        { firstname: 'VedaGna', lastname: 'Yarasu' },
+        { firstname: 'Gnapika', lastname: 'Yarasu' },
+        { firstname: 'Hindu', lastname: 'Sajja' },
+    ],
+    fruits: [
+        { name: 'oranges', type: 'citrus' },
+        { name: 'strawberries', type: 'berries' },
+        { name: 'watermelons', type: 'melons' },
+        { name: 'tomatoes', type: 'vegies' },
+    ]
 
-let fullname = `${name} ${surename}`;
-console.log(fullname); // VedaGna Yarasu
+}
+myObject.people.forEach((person) => {
+    console.log(person);
+    output.innerHTML += `<h4>${person.firstname} ${person.lastname}</h4>`
+})
+
+myObject.fruits.forEach((ele) => {
+    console.log(Object.keys(ele));
+})
